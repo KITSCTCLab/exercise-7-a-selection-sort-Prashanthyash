aba2c694d295e765dec0b96058f623b3e8d84873
Prashanthyash <110010652+Prashanthyash@users.noreply.github.com>
2022-09-13 11:13:19 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,14 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
-  # Write your code here
   for step in range(size):
-    min-idx= step
-    for i in range(step+1,size):
-      if array [i]<array[min-idx]:
-        min-idx= i
-        (array[step],array [min-index])=(array[min-idx],array[step])
-         return array
-       
+    min_idx = step
+    for i in range(step + 1, size):
+      if array[i] < array[min_idx]:
+        min_idx = i
+    (array[step], array[min_idx]) = (array[min_idx], array[step])
+  return array
+
 # Do not change the following code
 input_data = input()
 data = []
